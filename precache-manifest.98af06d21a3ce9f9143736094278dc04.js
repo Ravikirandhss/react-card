d244593ab9c3fa4aed9a269a45e79a869432be9d
Ravikirandhss <dhss.ravikiran@gmail.com>
2020-06-23 21:46:05 +0530

    Updates

---
+++ precache-manifest.98af06d21a3ce9f9143736094278dc04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc9e0bd05721a84025603da378ede1b",
+    "url": "/react-card/index.html"
+  },
+  {
+    "revision": "0b2a718da139c44891bb",
+    "url": "/react-card/static/css/main.4d26ffb0.chunk.css"
+  },
+  {
+    "revision": "befb3fc69b41815ac5a2",
+    "url": "/react-card/static/js/2.68166207.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-card/static/js/2.68166207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2a718da139c44891bb",
+    "url": "/react-card/static/js/main.bbb97b23.chunk.js"
+  },
+  {
+    "revision": "8080a0e9644ff3326dec",
+    "url": "/react-card/static/js/runtime-main.0823311d.js"
+  },
+  {
+    "revision": "cb0caa49c4e5330f47af75295fe5d031",
+    "url": "/react-card/static/media/icon.cb0caa49.jpeg"
+  }
+]);
